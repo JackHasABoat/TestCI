59ae6f6b20b944e7a38fb60354e2e86daa1cfe7f
JackHasABoat <JackHasABoat@gmail.com>
2020-03-12 14:28:58 +1300

    add more debug output

--- myapp/app.py
+++ myapp/app.py
@@ -1,3 +1,7 @@
 # Simple Code
 def add_function(param1, param2):
+    print("This is the add function")
     return param1 + param2
+
+if __name__ == '__main__':
+    print("This is the main function")
